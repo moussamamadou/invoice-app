72a87efab40cb69f92ae1b8ed863ec3e5ca0014d
Moussa Mamadou <87030270+moussamamadou@users.noreply.github.com>
2021-08-22 23:00:24 +0200

    Add API call for users and invoices

---
+++ frontend/pages/api/invoice.js
@@ -0,0 +1,67 @@
+import nc from 'next-connect';
+import { createStrapiAxios } from '../../utils/strapi';
+import axios from 'axios';
+
+
+export async function fetchAPI(path, user) {
+  try{
+  const response = await createStrapiAxios(user)
+    .get(path)
+    .then(res => res.data)
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getInvoices(user) {
+  try {
+    const invoices = await fetchAPI("/invoices", user);
+    return invoices;
+  }catch (error) {
+    console.log(error);
+  }
+}
+
+  export async function getInvoice(user, id) {
+    try {
+      const invoice = await fetchAPI(`/invoices/${id}`, user);
+      return invoice;
+    }catch (error) {
+      console.log(error); 
+    }
+  }
+  
+  export async function addInvoice(user, data) {
+    try{
+      const response = await createStrapiAxios(user)
+        .post(`/invoices`, data)
+        .then(res => res.data)
+        return response;
+      } catch (error) {
+        console.log('addInvoice --', error);
+      }
+  }
+  
+  export async function updateInvoice(user,id, data) {
+    console.log(data)
+    try{
+      const response = await createStrapiAxios(user)
+        .put(`/invoices/${id}`, data)
+        .then(res => res.data)
+        return response;
+      } catch (error) {
+        console.log('updateInvoice --',error);
+      }
+  }
+
+  export async function deleteInvoice(user,id) {
+    try{
+      const response = await createStrapiAxios(user)
+        .delete(`/invoices/${id}`)
+        .then(res => res.data)
+        return response;
+      } catch (error) {
+        console.log('deleteInvoice --',error);
+      }
+  }
---
+++ frontend/pages/api/users.js
@@ -0,0 +1,34 @@
+import nc from 'next-connect';
+import { sessionMiddleware } from '../../middlewares/session';
+import { createStrapiAxios } from '../../utils/strapi';
+
+export default nc()
+.use(sessionMiddleware)
+  .post(async (req, res) => {
+    const { id, username, email, address} = req.body;
+    const userSession = req.session.get('user');
+    try {
+      const user = await createStrapiAxios(userSession)
+        .put(`/users/${id}`, {username, email, address})
+        .then((res) => res.data)
+        .then((data) => ({
+          // ...data,
+          id : data.id,
+          username : data.username,
+          email : data.email,
+          address : data.address,
+          confirmed : data.username,
+          profileImage : data.username,
+          strapiToken: req.session.get('user').strapiToken,
+        })).catch(error => console.error(`Axios PUT /users/${id} - `, error))
+
+        console.log("rrrr", user)
+      req.session.set('user', user);
+      await req.session.save();
+      res.json(user);
+    } catch (error) {
+      console.log(error)
+    }
+  });
+
+  
