53be3564330c5736925bfc266ee5ac007efc7e04
Moussa Mamadou <87030270+moussamamadou@users.noreply.github.com>
2021-08-22 23:02:11 +0200

    Updating InvoiceEditor to handle Add and Update
    InvoiceEditor can :
    - handle add Invoice when displayed in the home page
    handle update Invoice when displayed in the invoice page

--- frontend/components/InvoiceEditor.js
+++ frontend/components/InvoiceEditor.js
@@ -2,40 +2,48 @@ import { Form, Formik } from 'formik';
 import React, { useState,useEffect } from 'react'
 import {MyInput, MySelect, MyAddressInput, MyItemInput} from "./customField"
 import * as Yup from 'yup'
+import { useRouter } from 'next/router';
+import { updateInvoice, addInvoice } from "../pages/api/invoice"
 
 const now = () => `${new Date().getFullYear()}-${('0' + new Date().getMonth()).slice(-2)}-${new Date().getDate()}`
 
 const initialValues = { 
-  streetFrom: '', 
-  cityFrom: '', 
-  postCodeFrom: '', 
-  countryFrom: '',
+  addressFrom: {
+   street: "", 
+   city: "", 
+   postCode: '', 
+   country: '',
+  },
+  addressTo:{         
+   street: '', 
+   city: '', 
+   postCode: '', 
+   country: '',
+  },
   clientName: '',
-  email: '',
-  streetTo: '', 
-  cityTo: '', 
-  postCodeTo: '', 
-  countryTo: '',
+  clientEmail: '',
   invoiceDate: now(),
   paymentTerms: 1,
   projectDescription: '',
-  itemList: [
+  items: [
     { 
       name: '', 
       quantity:1, 
       price: 0.00
     }
   ],
-  status: 'draft'
+  status: 'draft',  
+  reference: '',
+  author: {id : ''}
 };
 
 const invoiceSchema = Yup.object().shape({
   clientName: Yup.string().min(4, 'At 4 least Character').required(`Client's name is required`),
-  email: Yup.string().email('Invalid email').required(`Client's mail is required`),
+  clientEmail: Yup.string().email('Invalid email').required(`Client's mail is required`),
   invoiceDate: Yup.date().required('Issue Date is required'),
   paymentTerms: Yup.number('Payment Terms most be a number of days').required('Payments Terms is required'),
   projectDescription: Yup.string().min(8, 'At 8 least Character').required('Project Description is required'),
-  itemList: Yup.array()
+  items: Yup.array()
             .of(
               Yup.object().shape({
                 name:Yup.string().required(),
@@ -48,30 +56,94 @@ const invoiceSchema = Yup.object().shape({
   status: Yup.string().required()
 })
 
-export default function InvoiceEditor({isNew, invoiceValues = initialValues}) {
+export default function InvoiceEditor({isNew, invoice, user }) {
+  const router = useRouter();
   
-  const handleSubmit = (values, {setSubmitting, resetForm, setFieldValue}) => {
+  const [typeEditor, setTypeEditor] = useState('add');
+
+  let invoiceValue;
+  if (invoice){
+     invoiceValue = { 
+       addressFrom: {
+        street: invoice.addressFrom?.street || '', 
+        city: invoice.addressFrom?.city || '', 
+        postCode: invoice.addressFrom?.postCode || '', 
+        country: invoice.addressFrom?.country || '',
+       },
+       addressTo:{         
+        street: invoice.addressTo?.street || '', 
+        city: invoice.addressTo?.city || '', 
+        postCode: invoice.addressTo?.postCode || '', 
+        country: invoice.addressTo?.country || '',
+       },
+      clientName: invoice.clientName,
+      clientEmail: invoice.clientEmail,
+      invoiceDate: invoice.invoiceDate,
+      paymentTerms: invoice.paymentTerms,
+      projectDescription: invoice.projectDescription,
+      items: invoice.items?.map(item => ({
+        name: item.name,
+        quantity: item.quantity,
+        price: item.price
+      })),
+      status: invoice.status,
+      reference: invoice.reference,
+      id: invoice.id,
+      author: {id : invoice.author.id},
+    };
+  }else {
+     invoiceValue = initialValues;
+     invoiceValue.author.id = user.id;
+  }
+
+
+
+  const handleSubmit = (values, {setSubmitting, resetForm}) => {
+        
+    const data = {
+      addressFrom : values.addressFrom,
+      addressTo : values.addressTo,
+      clientName: values.clientName,
+      clientEmail: values.clientEmail,
+      invoiceDate: values.invoiceDate,
+      paymentTerms: values.paymentTerms,      
+      projectDescription: values.projectDescription,
+      reference: values.reference,
+      status: values.status,
+      author: values.author,
+      items: values.items
+    }
+
+    if (typeEditor === 'add'){
+      const response = addInvoice(user, data)
+        .then(res => router.push(`/invoice/${res.id}`))
+        .catch(error => console.log(res))   
+    } else if (typeEditor === 'update'){
+      const response = updateInvoice(user, values.id, data)
+        .then(res => router.reload(`/invoice/${values.id}`))
+        .catch(error => console.log(res))   
+    }
+
     setSubmitting(false);
     resetForm();    
   }
 
   return (
     <div className="">
-        <h2>{isNew ? `New Invoice` : `Edit #${invoiceValues.reference}`}</h2>
+        <h2>{isNew ? `New Invoice` : `Edit #${invoice.reference}`}</h2>
         <Formik             
-          initialValues={initialValues}
+          initialValues={invoiceValue}
           validationSchema={invoiceSchema}
           onSubmit={handleSubmit}
         >
           {({values, ...props}) => (
-            <Form>    
-                   
+            <Form>                       
               <h4 className="color-primary">Bill From</h4>
               <MyAddressInput 
-                street="streetFrom" 
-                postCode="postCodeFrom" 
-                city="cityFrom" 
-                country="countryFrom"
+                street="addressFrom.street" 
+                postCode="addressFrom.postCode" 
+                city="addressFrom.city" 
+                country="addressFrom.country" 
               />
               <h4 className="color-primary">Bill To</h4>
               <MyInput 
@@ -81,20 +153,14 @@ export default function InvoiceEditor({isNew, invoiceValues = initialValues}) {
               />
               <MyInput 
                 label="Client's Email"
-                name="email"
+                name="clientEmail"
                 type="email"
               />
               <MyAddressInput 
-                street="streetTo" 
-                postCode="postCodeTo" 
-                city="cityTo" 
-                country="countryTo"
-              />
-              <MyAddressInput 
-                street="streetFrom" 
-                postCode="postCodeFrom" 
-                city="cityFrom" 
-                country="countryFrom" 
+                street="addressTo.street" 
+                postCode="addressTo.postCode" 
+                city="addressTo.city" 
+                country="addressTo.country"
               />
               <MyInput
                 label="Issue Date"
@@ -111,7 +177,7 @@ export default function InvoiceEditor({isNew, invoiceValues = initialValues}) {
                 type="text"
               />
               <h3>Item List</h3> 
-              <MyItemInput itemList={values.itemList}/>
+              <MyItemInput items={values.items}/>
               {
                 isNew ? 
                   <div className="btn-primary-primary-primary-primary-primary-wrapper">
@@ -124,14 +190,22 @@ export default function InvoiceEditor({isNew, invoiceValues = initialValues}) {
                     <button 
                       type="submit" 
                       className="btn-primary" 
-                      onClick={() => props.setFieldValue('status', 'draft')}
+                      onClick={() => {
+                        setTypeEditor('add');
+                        props.setFieldValue('reference',(Math.random().toString(36).substring(2,4) +(Math.floor(Math.random() * 10000) + 10000).toString().substring(1)).toUpperCase());
+                        props.setFieldValue('status', 'draft')
+                      }}
                     >
                       Save as Draft
                     </button>
                     <button 
                       type="submit" 
                       className="btn-primary" 
-                      onClick={() => props.setFieldValue('status', 'pending')}
+                      onClick={() => {
+                        setTypeEditor('add');
+                        props.setFieldValue('reference',(Math.random().toString(36).substring(2,4) +(Math.floor(Math.random() * 10000) + 10000).toString().substring(1)).toUpperCase());
+                        props.setFieldValue('status', 'pending')
+                      }}
                     >
                       Save & Send
                     </button>
@@ -148,6 +222,7 @@ export default function InvoiceEditor({isNew, invoiceValues = initialValues}) {
                     <button 
                       type="submit" 
                       className="btn-primary" 
+                      onClick={() => setTypeEditor('update')}
                     >
                       Save Changes
                     </button>
