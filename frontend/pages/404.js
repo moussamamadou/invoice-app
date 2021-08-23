dd032c99e588fadca0b081aae373cc84c8887a21
Moussa Mamadou <87030270+moussamamadou@users.noreply.github.com>
2021-08-23 11:44:17 +0200

    Create 404.js

---
+++ frontend/pages/404.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import PublicLayout from "../components/PublicLayout"
+
+export default function FourOhFour() {
+  return(
+    <PublicLayout>
+        <div className="404-container">
+            <h1>404 - Page Not Found</h1>
+            <div className="is-empty">
+                <img src="/assets/illustration-empty.svg" alt="" />
+                <h2>There is nothing here</h2>
+                <p>You are lost in the parallel invoice dimension</p>        
+                <Link href="/"><a>Go back home</a></Link>
+            </div>
+        </div>
+    </PublicLayout>
+  )
+}
