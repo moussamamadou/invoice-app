ff51ea239d3a9bc7adbd0365eebf8cf93af9a37e
Moussa Mamadou <87030270+moussamamadou@users.noreply.github.com>
2021-08-27 09:06:25 +0200

    Correction for heroku database and server

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -18,7 +18,7 @@ module.exports = ({ env }) => {
           ssl: { rejectUnauthorized: false }
         },
         options: {
-          ssl: false
+          ssl: true
         },
       },
     },
